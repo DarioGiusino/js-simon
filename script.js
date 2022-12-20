d0ab41222df7b8d36b2cfe1fea860d02d47bbdd7
DarioGiusino <dario.giusino@gmail.com>
2022-12-20 18:20:07 +0100

    solved exercise

--- script.js
+++ script.js
@@ -10,8 +10,42 @@ Esiste un oggetto JS in grado di gestire le date?
 Esistono dei metodi per trasformare una data in millisecondi?
 */
 
+// # funzioni
+// funzione per trasformare millisecondi in intero
+function getRest(a, b, c) {
+    const math = Math.floor((a % b) / c);
+    return math;
+}
+
+function timer() {
+    const today = new Date().getTime();
+
+    const msLeftToXmas = xmasDate - today;
+    if (msLeftToXmas > 0){
+        daysElement.innerText = Math.floor(msLeftToXmas / days);
+        hoursElement.innerText = getRest(msLeftToXmas, days, hours);
+        minutesElement.innerText = getRest(msLeftToXmas, hours, minutes);
+        secondsElement.innerText = getRest(msLeftToXmas, minutes, seconds);
+    } else {
+        clearInterval(timer);
+    }
+}
+
 // # prendo elementi dal dom
 const daysElement = document.getElementById('days');
 const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const secondsElement = document.getElementById('seconds');
+
+// # fase preliminare
+// prendo data ultima, secondi, minuti, ore e giorni in ms
+const xmasDate = new Date('25 December 2022').getTime();
+const seconds = 1000;
+const minutes = 60 * seconds;
+const hours = 60 * minutes;
+const days = 24 * hours;
+
+// # avvio contatore
+setInterval(timer, 1000)
+
+
