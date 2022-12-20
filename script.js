76e4c0a6c0160661af55add1505c27af4530f99e
DarioGiusino <dario.giusino@gmail.com>
2022-12-20 16:35:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+In pieno tema natalizio sfruttiamo le timing functions per  fare il conto alla rovescia per il Natale!  Ogni secondo il nostro countdown dovrà scalare fino alla mezzanotte del 25 dicembre!
+Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
+Da quante ore è composto un giorno? 24
+Da quanti minuti è composta un'ora? 60
+Da quanti secondi è composto un minuto? 60
+Da quanti millisecondi è composto un secondo? 1000
+Quanti millisecondi mi separano dal Natale?
+Esiste un oggetto JS in grado di gestire le date?
+Esistono dei metodi per trasformare una data in millisecondi?
+*/
+
+// # prendo elementi dal dom
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
